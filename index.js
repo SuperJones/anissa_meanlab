8423b6089a62d520a6eefaca0de7a8a3f6943e1b
Anissa Jones <anissajones10@gmail.com>
2016-04-14 11:32:59 -0400

    requires my encode/decode function.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express     = require("express");
 var hbs         = require("express-handlebars");
-
+var path        = require("path"); //helps make path manipulation easier.
 var app         = express();
+var base58      = require("./base58.js");
 
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
@@ -11,9 +12,21 @@ app.engine(".hbs", hbs({
   defaultLayout:  "layout-main"
 }));
 
+//tells Express to serve files from our public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", function(req, res){
+  //route to serve up the homepage
   res.render("index-view");
 });
+
+// app.post("", function(req, res){
+//
+// });
+//
+// app.get("", function(req, res){
+//
+// });
 
 app.listen(3001, function(){
   console.log("Your server is aliiive!!");
