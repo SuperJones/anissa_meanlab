3beaa651d3a5319626242dcf16f93ee3072f8a3e
Anissa Jones <anissajones10@gmail.com>
2016-04-14 12:16:20 -0400

    Makes Schemas and models for counter and url.

---
+++ models/url.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+/*Need a counter collection because Mongodb has no autoincrementer
+but by doing it this way we can take advantage of creating an id
+and pulling that into the main table.*/
+var CounterSchema = Schema({
+  _id:  { type: String, required: true},
+  seq:  { type: Number, default: 0}
+});
+
+//schema for my url links
+var UrlSchema = Schema({
+  _id: {type: Number, index: true},
+  long_url: String,
+  created_at: Date
+});
+
+// The pre('save', callback) executes the callback function
+// every time before an entry is saved to the urls collection.
+UrlSchema.pre('save', function(next){
+  var doc = this;
+  // find the url_count and increment it by 1
+  //$inc takes an object of the property and how many to increment it by.
+  counter.findByIdAndUpdate({_id: 'url_count'}, {$inc: {seq: 1} }, function(error, counter) {
+      if (error)
+          return next(error);
+      // set the _id of the urls collection to the incremented value of the counter
+      doc._id = counter.seq;
+      doc.created_at = new Date();
+      next();
+  });
+});
+
+//make a counter model
+var counter = mongoose.model("counter", CounterSchema);
+//make a Url model
+var Url = mongoose.model("Url", UrlSchema);
+
+//export Url
+module.exports = Url;
