7630cbd23dada9ff50bbab2a96619c7bd7cdbc20
Anissa Jones <anissajones10@gmail.com>
2016-04-14 15:50:09 -0400

    Uses jquery/ajax to make an event listener and deal with displaying shortened url.

--- public/javascript/shorten.js
+++ public/javascript/shorten.js
@@ -7,6 +7,11 @@ $('.btn-shorten').on('click', function(){
     dataType: 'JSON',
     //gets the value of what was typed in the input field.
     data: {url: $('#url-field').val()},
-    success: function(){}
-  })
+    success: function(data){
+      // display the shortened URL to the user that is returned by the server
+      var resultHTML = '<a href="' + data.shortURL + '">' + data.shortURL + '</a>';
+      $('#link').html(resultHTML);
+      $('#link').hide().fadeIn('slow');
+    }
+  });
 });
